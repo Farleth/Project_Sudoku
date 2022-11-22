b9a6791b8bad8b1802c30abc7196b3e403575b06
eliana cuhadar <eliana.cuhadar.69@hotmail.com>
2022-11-22 14:15:26 +0100

    Initialisation d'une liste de 81 objets de type Cell pour chaque coordonées. + Ajout de la liste possible_values dans la class Cell contenant les chiffres de 1 a 9.

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 
-class Cell:
+class Cell():
 
     def __init__(self, x_coords: int, y_coords : int):
         self.y_coords = y_coords
         self.value : int = 0
         self.x_coords = x_coords
         self.group : int = ((x_coords//3) * 3) + y_coords//3
+        self.possible_values = [1,2,3,4,5,6,7,8,9]
 
     def __str__(self) -> str:
-        return print(self.value)
+        return f'value={self.value},x= {self.x_coords}, y={self.y_coords}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
